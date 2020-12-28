fb64ecf37701a8a4003c4101280e56fceefb1b5a
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2020-12-29 00:31:51 +0530

    Add files via upload

---
+++ regular_seq.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+       string s;
+       cin>>s;
+       if(s[0]==')' || s[s.length()-1]=='(' || (s.length())%2==1)
+          cout<<"No"<<endl;
+       else
+          cout<<"Yes"<<endl;
+    }
+}
